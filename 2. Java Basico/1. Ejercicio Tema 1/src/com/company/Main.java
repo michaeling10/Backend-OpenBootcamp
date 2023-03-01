e6b074ef8df2784769bafebdee7598297c806151
michaeling10 <michaeling10@hotmail.com>
2023-02-28 21:50:14 -0500

    Java Basico - Ejercicio 1

---
+++ 2. Java Basico/1. Ejercicio Tema 1/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        byte Byte = 2;
+        short Short = 3;
+        int Int = 4;
+        long Long = 5;
+        float Float = 6.5f;
+        double Double = 7.5d;
+        boolean Boolean = true;
+        char Char = 'a';
+        String String = "Texto de referencia";
+
+        System.out.println("Byte :" + Byte);
+        System.out.println("Short :" + Short);
+        System.out.println("Int :" + Int);
+        System.out.println("Long :" + Long);
+        System.out.println("Float :" + Float);
+        System.out.println("Double :" + Double);
+        System.out.println("Boolean :" + Boolean);
+        System.out.println("Char :" + Char);
+        System.out.println("String :" + String);
+    }
+}
